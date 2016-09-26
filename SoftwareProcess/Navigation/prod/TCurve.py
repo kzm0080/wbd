ba2d859545465f78aa695f6d7297fd3cb1bb69c6
Kirankumar <Kirankumar@Kiran>
2016-09-25 19:14:58 -0500

    Final Commit of TDD curve

--- SoftwareProcess/Navigation/prod/TCurve.py
+++ SoftwareProcess/Navigation/prod/TCurve.py
@@ -61,9 +61,54 @@ class TCurve(object):
         result = base ** exponent
         return result
     
-    def integrate(self, t, n, f):        
-        pass
+    def integrate(self, t, n, f):     
+        epsilon = 0.001  # Declaring epsilon
+        simpsonOld = 0.0
+        simpsonNew = epsilon  # Assigning epsilon
+        lowBound = 0  # Low bound
+        highBound = t  # High bound
+        w=0.0
+        s = 4  # No of slices of our choice
+        while (abs((simpsonNew - simpsonOld) / simpsonNew) > epsilon):
+            simpsonOld = simpsonNew 
+            sArea = (highBound - lowBound)  # Get high bound           
+            w = (sArea) / s
+            sOne = f(lowBound, n)  # Finding first term
+            
+            sMiddle = 0.0 # Add all middle terms
+            
+            sStart = 1
+            sEO = "O"
+            
+            while(sStart < s): # Starting loop along with no of slices
+                if(sEO == "O"):
+                    sMiddle += self.getTerm(n, lowBound, sStart, w, sEO,f) # Get the odd values                    
+                    sEO = "E"    
+                    sStart = sStart + 1     
+                else:
+                    sMiddle += self.getTerm(n, lowBound, sStart, w, sEO,f)  # Get the even values
+                    sEO = "O" 
+                    sStart = sStart + 1 # Increment values
+                
+            sEnd = f(highBound, n)  # Finding last term
+            sAll = 0
+            sAll = sOne + sMiddle + sEnd
+            simpsonNew = sAll * (w / 3.0)  # simpsons rule integration
+            s = s * 2  # doubles the number of slices   
         
+        return simpsonNew         
+    
+    # Get the odd and even values
+        
+    def getTerm(self, n, lowBound, i, w, term,f):
+        sOdd=0.0
+        if(term == "O"):  # If odd          
+            sOdd = (i * w)
+            sTerm = (4.0 * f(sOdd, n)) # Multiply with 4 for odd terms
+        else: # If even
+            sEven = (i * w)
+            sTerm = (2.0 * f(sEven, n)) # Multiply with 4 for even terms
+        return sTerm    
         
     
         
